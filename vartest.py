d526f4409723cba2fc69e57b156d8b952efd58ec
seedumfamily <seedumfamily@yahoo.com>
2021-01-18 07:29:32 -0500

    added vartest file

---
+++ vartest.py
@@ -0,0 +1,35 @@
+a=10
+b=10.5
+name="ram"
+name1='raju'
+x=True
+
+print(a)
+print(b)
+print(name)
+print(name1)
+print(a+b)
+print(x)
+
+#this statement assigned multiple cariables in in single line
+a, b, name = 10, 10.5, "raju"
+print(a,b, name)
+
+a,b = 10, 10
+print("both values")
+a=b=10
+print(a,b)
+################################################
+x=1
+y=2
+
+y,x = x, y #assign x to y, y value to x
+
+print(x, y)
+
+
+
+
+
+
+
